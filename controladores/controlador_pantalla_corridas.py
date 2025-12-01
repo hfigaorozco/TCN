a679b39daf85e1c69ad86726c7ee646d24ccde6a
GaliDaVinci <leonardovaldez583@gmail.com>
2025-12-01 13:55:49 -0800

    feat: Add filters for corrida number and origin/destination cities to table

--- controladores/controlador_pantalla_corridas.py
+++ controladores/controlador_pantalla_corridas.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import QTableWidgetItem, QTableWidget, QLineEdit, QComboBox # Added QComboBox
+from PySide6.QtWidgets import QTableWidgetItem, QTableWidget, QLineEdit, QComboBox
 
 class ControladorPantallaCorridas:
     def __init__(self, corrida_dao):
@@ -10,6 +10,7 @@ class ControladorPantallaCorridas:
         # Filter states
         self.filtro_numero_corrida = ""
         self.filtro_origen = ""
+        self.filtro_destino = "" # New: Filter state for destination
 
     def set_vista(self, vista_corridas):
         self.vista = vista_corridas
@@ -29,12 +30,19 @@ class ControladorPantallaCorridas:
         else:
             print("Error: lineEdit_buscarNumCorr not found in the UI.")
         
-        # New: Find and connect comboBox_filtroOrigenCorr
+        # Find and connect comboBox_filtroOrigenCorr
         self.comboBox_filtroOrigenCorr = self.vista.ui.findChild(QComboBox, 'comboBox_filtroOrigenCorr')
         if self.comboBox_filtroOrigenCorr:
             self.comboBox_filtroOrigenCorr.currentIndexChanged.connect(self._on_filtro_origen_changed)
         else:
             print("Error: comboBox_filtroOrigenCorr not found in the UI.")
+        
+        # New: Find and connect comboBox_filtroDestinoCorr
+        self.comboBox_filtroDestinoCorr = self.vista.ui.findChild(QComboBox, 'comboBox_filtroDestinoCorr')
+        if self.comboBox_filtroDestinoCorr:
+            self.comboBox_filtroDestinoCorr.currentIndexChanged.connect(self._on_filtro_destino_changed)
+        else:
+            print("Error: comboBox_filtroDestinoCorr not found in the UI.")
 
         self.tabla_corridas.setColumnCount(10)
         self.tabla_corridas.setHorizontalHeaderLabels([
@@ -80,8 +88,17 @@ class ControladorPantallaCorridas:
             self.filtro_origen = self.comboBox_filtroOrigenCorr.currentText()
             # If the first item is a "select all" or "none" option, treat it as no filter
             # Assuming "Seleccione..." or similar is the first item
-            if index == 0 and (self.filtro_origen == "Seleccione..." or self.filtro_origen == "All"): # Adjusted for potential default/all options
+            if index == 0 and (self.filtro_origen == "Seleccione..." or self.filtro_origen == "All" or self.filtro_origen == ""):
                 self.filtro_origen = ""
+            self._aplicar_filtros()
+
+    # New: Method to handle changes in destination combobox
+    def _on_filtro_destino_changed(self, index):
+        if self.comboBox_filtroDestinoCorr:
+            self.filtro_destino = self.comboBox_filtroDestinoCorr.currentText()
+            # If the first item is a "select all" or "none" option, treat it as no filter
+            if index == 0 and (self.filtro_destino == "Seleccione..." or self.filtro_destino == "All" or self.filtro_destino == ""):
+                self.filtro_destino = ""
             self._aplicar_filtros()
 
     def _aplicar_filtros(self):
@@ -103,6 +120,13 @@ class ControladorPantallaCorridas:
             corridas_filtradas = [
                 corrida for corrida in corridas_filtradas
                 if corrida['ciudad_origen'] == self.filtro_origen
+            ]
+
+        # New: Apply destino filter
+        if self.filtro_destino:
+            corridas_filtradas = [
+                corrida for corrida in corridas_filtradas
+                if corrida['ciudad_destino'] == self.filtro_destino
             ]
         
         self._actualizar_tabla(corridas_filtradas)
